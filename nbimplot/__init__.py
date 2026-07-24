3659b3cfabf76fc57d300c92eae8fad9ec08c922
root <root@m174.irageauth.io>
2026-07-24 12:21:08 +0530

    Expose package version

--- nbimplot/__init__.py
+++ nbimplot/__init__.py
@@ -1,5 +1,12 @@
 """nbimplot public API."""
 
+from importlib.metadata import PackageNotFoundError, version
+
 from ._plot import AlignedPlots, LineHandle, Plot, Subplots
 
-__all__ = ["Plot", "LineHandle", "Subplots", "AlignedPlots"]
+try:
+    __version__ = version("nbimplot")
+except PackageNotFoundError:
+    __version__ = "0.1.9"
+
+__all__ = ["Plot", "LineHandle", "Subplots", "AlignedPlots", "__version__"]
